7a2290dd3c88e985c324059dbacc5977d182e34a
Everton Dias <esrdias@gmail.com>
2020-08-27 22:20:23 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<!-- Meta Tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/global.css">
+
+	<title>Hello, world!</title>
+</head>
+<body>
+	<h1>Hello, world</h1>
+</body>
+</html>
