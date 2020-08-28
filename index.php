c3644393b6274a3c6671fc85db288436c746e6db
Everton Dias <esrdias@gmail.com>
2020-08-28 09:54:49 -0300

    Iniciando desenvolvimento do menu

--- index.php
+++ index.php
@@ -10,10 +10,49 @@
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="css/global.css">
+	<link rel="stylesheet" href="css/menu.css">
 
 	<title>Hello, world!</title>
 </head>
 <body>
-	<h1>Hello, world</h1>
+	<div id="menu">
+		<header>
+			<div class="logo">
+				Logo vai aqui
+			</div>
+			<div class="user-info">
+				Igor Câmara | (4) produtos
+			</div>
+		</header>
+		<div class="search">
+			<input type="text" placeholder="Pesquise aqui por um livro">
+		</div>
+		<div class="categories-button">
+			+ Categorias
+		</div>
+		<div class="categories">
+			<li>Ação</li>
+			<li>Antologias</li>
+			<li>Autoajuda</li>
+			<li>Aventura</li>
+			<li>Biografia</li>
+			<li>Ciência</li>
+			<li>Contos</li>
+			<li>Crônica</li>
+			<li>Drama</li>
+			<li>Épico</li>
+			<li>Fantasia</li>
+			<li>Ficção Ciêntifica</li>
+			<li>Ficção Histórica</li>
+			<li>Horror</li>
+			<li>Infantojuvenil</li>
+			<li>Infantil</li>			
+			<li>Romance</li>
+		</div>
+	</div>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/js/all.min.js"
+		integrity="sha512-M+hXwltZ3+0nFQJiVke7pqXY7VdtWW2jVG31zrml+eteTP7im25FdwtLhIBTWkaHRQyPrhO2uy8glLMHZzhFog=="
+		crossorigin="anonymous"></script>
 </body>
 </html>
