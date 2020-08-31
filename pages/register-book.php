9a3b708e8facd92a502f2156390de2ecfcf89828
Everton Dias <esrdias@gmail.com>
2020-08-31 09:25:32 -0300

    Refatorando CSS

--- pages/register-book.php
+++ pages/register-book.php
@@ -16,29 +16,32 @@
 	<title>Hello, world!</title>
 </head>
 <body>
-
+	<div class="container">
 	<input class="input-search" type="text" id="search-book-api" />
 
-	<div class="books">
-		<div class="book">
-			<img src="../images/livro.jpg" class="img-livro">
-			<div class="infos">
-				<h1>Harry Potter</h1>
-				<h2>Lucas Hernandes</h2>
-				<div maxlength="100" class="description">
-					Hitler cometeu dois erros de estratégia que mudaram o rumo da Segunda Grande Guerra. O primeiro foi subestimar o senso patriótico dos britânicos, ao acreditar numa Inglaterra aliada aos interesses do Terceiro Reich — o que atrasou seus ataques maciços à ilha. O segundo, muito mais grave, lhe custou a vitória: a Operação Barbarossa. Uma série de intrincadas ações militares que supostamente levaria os nazistas aos portões de Moscou e à conquista da Rússia. Em Stalingrado — que rapidamente alcançou o topo das listas de mais vendidos em todos os países em que foi publicado e recebeu o prêmio Samuel Johnson de Não-Ficção de 1998 —, o historiador Antony Beevor trata da batalha de mesmo nome, onde a máquina de guerra nazista teve sua primeira derrota diante do exército soviético, que resistiu bravamente numa cidade em ruínas. Em agosto de 1942, o exército alemão chegou a Stalingrado e levantou um cerco que duraria pouco mais de um ano. Batizada em homenagem ao ditador e então líder soviético Stalin, a cidade, às margens do rio Volga era, ao mesmo tempo, um alvo moral e estratégico. Seria um duro golpe no orgulho russo ver a cidade com o nome de seu dirigente cair. Mais grave: Stalingrado era a porta para vários poços de petróleo ambicionados por Hitler. Stalingrado mostra como o temido contingente alemão estava completamente despreparado para o tipo de combate que se seguiu. Acostumado com ataques-relâmpago — que lhe rendeu o controle sobre a Polônia e a França —, Hitler não preparou seus homens para uma guerra corpo a corpo que pudesse levar meses. O rigoroso inverno das estepes russas chegou inclemente, emperrando fuzis, congelando soldados até a morte, dilapidando provisões e tornando metade do equipamento inútil. Beevor se preocupa em mostrar os dois lados da batalha, justapondo e contrapondo pensamentos e estratégias tanto de soldados e oficiais alemães quanto de soviéticos. Stalingrado revela como o exército vermelho também carecia de treinamento e logística, o que torna sua resistência ainda mais impressionante. Durante os meses seguintes ao cerco, os oficiais russos espionavam tanto o exército inimigo quanto o seu próprio: vários jovens perderam a vida ao tentar desertar ou passar para as linhas inimigas. Antony Beevor entrevistou vários sobreviventes da Batalha de Stalingrado e descobriu novos e importantes documentos nos arquivos alemães e soviéticos, abertos após a queda do muro de Berlim — incluindo relatórios de interrogatórios, deserções e execuções de prisioneiros. Além de ordens soviéticas para que diversas cidades fossem destruídas a fim de dificultar ao máximo a vida do exército invasor. Um trabalho que prende o leitor em meio a lutas travadas em condições inumanas, ao lado de civis entrincheirados em um campo de batalha urbano.
+		<div class="books">
+			<div class="book">
+				<div class="image-div">
+					<img src="../images/livro.jpg" class="img-livro">
 				</div>
-				<div class="date">
-					2002
-				</div>
-				<div class="button-div">
-					<button class="pre-register">
-						Cadastrar
-					</button>
+				<div class="infos">
+					<h1>Harry Potter</h1>
+					<h2>Lucas Hernandes</h2>
+					<div class="description">
+						Hitler cometeu dois erros de estratégia que mudaram o rumo da Segunda Grande Guerra. O primeiro foi subestimar o senso patriótico dos britânicos, ao acreditar numa Inglaterra aliada aos interesses do Terceiro Reich — o que atrasou seus ataques maciços à ilha. O segundo, muito mais grave, lhe custou a vitória: a Operação Barbarossa. Uma série de intrincadas ações militares que supostamente levaria os nazistas aos portões de Moscou e à conquista da Rússia. Em Stalingrado — que rapidamente alcançou o topo das listas de mais vendidos em todos os países em que foi publicado e recebeu o prêmio Samuel Johnson de Não-Ficção de 1998 —, o historiador Antony Beevor trata da batalha de mesmo nome, onde a máquina de guerra nazista teve sua primeira derrota diante do exército soviético, que resistiu bravamente numa cidade em ruínas. Em agosto de 1942, o exército alemão chegou a Stalingrado e levantou um cerco que duraria pouco mais de um ano. Batizada em homenagem ao ditador e então líder soviético Stalin, a cidade, às margens do rio Volga era, ao mesmo tempo, um alvo moral e estratégico. Seria um duro golpe no orgulho russo ver a cidade com o nome de seu dirigente cair. Mais grave: Stalingrado era a porta para vários poços de petróleo ambicionados por Hitler. Stalingrado mostra como o temido contingente alemão estava completamente despreparado para o tipo de combate que se seguiu. Acostumado com ataques-relâmpago — que lhe rendeu o controle sobre a Polônia e a França —, Hitler não preparou seus homens para uma guerra corpo a corpo que pudesse levar meses. O rigoroso inverno das estepes russas chegou inclemente, emperrando fuzis, congelando soldados até a morte, dilapidando provisões e tornando metade do equipamento inútil. Beevor se preocupa em mostrar os dois lados da batalha, justapondo e contrapondo pensamentos e estratégias tanto de soldados e oficiais alemães quanto de soviéticos. Stalingrado revela como o exército vermelho também carecia de treinamento e logística, o que torna sua resistência ainda mais impressionante. Durante os meses seguintes ao cerco, os oficiais russos espionavam tanto o exército inimigo quanto o seu próprio: vários jovens perderam a vida ao tentar desertar ou passar para as linhas inimigas. Antony Beevor entrevistou vários sobreviventes da Batalha de Stalingrado e descobriu novos e importantes documentos nos arquivos alemães e soviéticos, abertos após a queda do muro de Berlim — incluindo relatórios de interrogatórios, deserções e execuções de prisioneiros. Além de ordens soviéticas para que diversas cidades fossem destruídas a fim de dificultar ao máximo a vida do exército invasor. Um trabalho que prende o leitor em meio a lutas travadas em condições inumanas, ao lado de civis entrincheirados em um campo de batalha urbano.
+					</div>
+					<div class="date">
+						2004
+					</div>
+					<div class="button-div">
+						<button class="pre-register">
+							Cadastrar
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
-
+	</div>
 	<!-- Optional JavaScript -->
 	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 	<script type="text/javascript" src="../js/api-key.js"></script>
