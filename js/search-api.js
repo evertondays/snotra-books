443340ef2b0bed474c350add65097578a8c8f83f
Everton Dias <esrdias@gmail.com>
2020-08-31 07:45:28 -0300

    Criando sistema de busca por API

---
+++ js/search-api.js
@@ -0,0 +1,89 @@
+const key = returnApikey();
+const url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+const inputSearch = document.querySelector('#search-book-api');
+const books = document.querySelector('.books');
+
+inputSearch.addEventListener('change', (event) => {
+	let busca = encodeURIComponent(event.target.value);
+	const query = url + busca + key;
+
+	books.innerHTML = "";
+
+	axios.get(query)
+		.then(function (response) {
+			const items = response.data.items;
+			
+			items.map((item) =>{
+				generateDiv(item);
+			})
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+})
+
+function generateDiv(item){
+	let book = document.createElement('div');
+	book.classList.add('book');
+	let infos = document.createElement('div');
+	infos.classList.add('infos');
+
+	let img = document.createElement('img');
+	img.classList.add('img-livro');
+	img.src = item.volumeInfo.imageLinks.thumbnail;
+	book.appendChild(img);
+
+	let h1 = document.createElement('h1');
+	h1.innerHTML = item.volumeInfo.title;
+	infos.appendChild(h1);
+
+	// Autores
+	const authors = item.volumeInfo.authors;
+	let h2 = document.createElement('h2');
+	
+	for (let i = 0; i < authors.length; i++){
+		if (i >= 1 && i != (authors.length - 1)){
+			h2.innerHTML += ", ";
+		}
+
+		if (i == (authors.length - 1) && i != 0){
+			h2.innerHTML += " e ";
+		}
+
+		h2.innerHTML += item.volumeInfo.authors[i];
+	}
+	infos.appendChild(h2);
+	// Fim autores
+
+	let description = document.createElement('div');
+	description.classList.add('description')
+	description.innerHTML = item.volumeInfo.description;
+	infos.appendChild(description);
+
+	// Data
+	let dateDiv = document.createElement('div');
+	dateDiv.classList.add('date');
+
+	if (typeof item.volumeInfo.publishedDate !== "undefined") {
+
+		let publishedDate = item.volumeInfo.publishedDate;
+		let dateText = publishedDate.split('-');
+		dateDiv.innerHTML = dateText[0];	
+	} else {
+		dateDiv.innerHTML = "sem data";
+	}
+	infos.appendChild(dateDiv);
+	// Fim data
+
+	let divButton = document.createElement('div');
+	divButton.classList.add('button-div');
+	let button = document.createElement('button');
+	description.classList.add('pre-register')
+	button.innerHTML = "Cadastrar";
+	divButton.appendChild(button);
+	infos.appendChild(divButton);
+
+	book.appendChild(infos);
+	books.appendChild(book);
+}
---
+++ pages/register-book.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<!-- Meta Tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="../css/global.css">
+	<link rel="stylesheet" href="../css/register-book.css">
+
+	<title>Hello, world!</title>
+</head>
+<body>
+
+	<input class="input-search" type="text" id="search-book-api" />
+
+	<div class="books">
+		<div class="book">
+			<img src="../images/livro.jpg" class="img-livro">
+			<div class="infos">
+				<h1>Harry Potter</h1>
+				<h2>Lucas Hernandes</h2>
+				<div maxlength="100" class="description">
+					Hitler cometeu dois erros de estratégia que mudaram o rumo da Segunda Grande Guerra. O primeiro foi subestimar o senso patriótico dos britânicos, ao acreditar numa Inglaterra aliada aos interesses do Terceiro Reich — o que atrasou seus ataques maciços à ilha. O segundo, muito mais grave, lhe custou a vitória: a Operação Barbarossa. Uma série de intrincadas ações militares que supostamente levaria os nazistas aos portões de Moscou e à conquista da Rússia. Em Stalingrado — que rapidamente alcançou o topo das listas de mais vendidos em todos os países em que foi publicado e recebeu o prêmio Samuel Johnson de Não-Ficção de 1998 —, o historiador Antony Beevor trata da batalha de mesmo nome, onde a máquina de guerra nazista teve sua primeira derrota diante do exército soviético, que resistiu bravamente numa cidade em ruínas. Em agosto de 1942, o exército alemão chegou a Stalingrado e levantou um cerco que duraria pouco mais de um ano. Batizada em homenagem ao ditador e então líder soviético Stalin, a cidade, às margens do rio Volga era, ao mesmo tempo, um alvo moral e estratégico. Seria um duro golpe no orgulho russo ver a cidade com o nome de seu dirigente cair. Mais grave: Stalingrado era a porta para vários poços de petróleo ambicionados por Hitler. Stalingrado mostra como o temido contingente alemão estava completamente despreparado para o tipo de combate que se seguiu. Acostumado com ataques-relâmpago — que lhe rendeu o controle sobre a Polônia e a França —, Hitler não preparou seus homens para uma guerra corpo a corpo que pudesse levar meses. O rigoroso inverno das estepes russas chegou inclemente, emperrando fuzis, congelando soldados até a morte, dilapidando provisões e tornando metade do equipamento inútil. Beevor se preocupa em mostrar os dois lados da batalha, justapondo e contrapondo pensamentos e estratégias tanto de soldados e oficiais alemães quanto de soviéticos. Stalingrado revela como o exército vermelho também carecia de treinamento e logística, o que torna sua resistência ainda mais impressionante. Durante os meses seguintes ao cerco, os oficiais russos espionavam tanto o exército inimigo quanto o seu próprio: vários jovens perderam a vida ao tentar desertar ou passar para as linhas inimigas. Antony Beevor entrevistou vários sobreviventes da Batalha de Stalingrado e descobriu novos e importantes documentos nos arquivos alemães e soviéticos, abertos após a queda do muro de Berlim — incluindo relatórios de interrogatórios, deserções e execuções de prisioneiros. Além de ordens soviéticas para que diversas cidades fossem destruídas a fim de dificultar ao máximo a vida do exército invasor. Um trabalho que prende o leitor em meio a lutas travadas em condições inumanas, ao lado de civis entrincheirados em um campo de batalha urbano.
+				</div>
+				<div class="date">
+					2002
+				</div>
+				<div class="button-div">
+					<button class="pre-register">
+						Cadastrar
+					</button>
+				</div>
+			</div>
+		</div>
+
+	<!-- Optional JavaScript -->
+	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+	<script type="text/javascript" src="../js/api-key.js"></script>
+	<script type="text/javascript" src="../js/search-api.js"></script>
+</body>
+</html>
