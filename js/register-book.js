42092dabe92e8023a6930059265c608c27a85bfe
Everton Dias <esrdias@gmail.com>
2020-09-05 00:44:48 -0300

    Adicionando função showRegisterDivByUser

--- js/register-book.js
+++ js/register-book.js
@@ -13,6 +13,24 @@ function showRegisterDivByAPI(callerId){
 	let authors = document.querySelector(`#${callerId} .infos h2`).innerHTML;
 	let description = document.querySelector(`#${callerId} .infos .description`).innerHTML;
 	let date = document.querySelector(`#${callerId} .infos .date`).innerHTML;
+	let imageUrl = document.querySelector(`#${callerId} .img-livro`).src;
+
+	document.querySelector('#input-title').value = title;
+	document.querySelector('#input-authors').value = authors;
+	document.querySelector('#input-description').value = description;
+	document.querySelector('#input-date').value = date;
+	document.querySelector('#input-img-url').value = imageUrl;
+
+	registerWrapper.style.display = 'grid';
+}
+
+function showRegisterDivByUser(){
+	// API Values
+	let image = '';
+	let title = '';
+	let authors = '';
+	let description = '';
+	let date = '';
 
 	document.querySelector('#input-title').value = title;
 	document.querySelector('#input-authors').value = authors;
