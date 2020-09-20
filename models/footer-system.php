e5a56c4aa9fc71d182c764f4b43e8e88c75065e7
Everton Dias <esrdias@gmail.com>
2020-09-19 23:28:32 -0300

    Criando rodapé para o sistema interno

---
+++ models/footer-system.php
@@ -0,0 +1,5 @@
+<?php // rodapé ?>
+<footer 
+	style=" bottom: 0; width: 100%; display: flex; justify-content: center; align-items: center; padding: 1rem; background-color: var(--primary); color: var(--white); font-size: 1.5rem; font-weight: bold; padding: 1rem;">
+	© Copyright 2020
+</footer>
