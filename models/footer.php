bd137d230bf50192553a560b2ae032764e45f1dc
Everton Dias <esrdias@gmail.com>
2020-09-23 14:29:21 -0300

    Criando footer

---
+++ models/footer.php
@@ -0,0 +1,37 @@
+<?php // Footer ?>
+<footer>
+		<div class="top">
+			<div class="institutional">
+				<h1>Sobre esse trabalho</h1>
+				Trabalho feito na instituição Etec Vasco Antônio Venchiarutti como requisito para obtenção de
+				nota na disciplina de Programação Web, solicitado pelos professores Monica Rezende e
+				Ronildo Ferreira.
+			</div>
+			<hr class="footer-line">
+			<div class="members">
+				<div class="member">
+					<h4>ELIAS SANTOS</h4>
+					<div class="text">Extrovertido e carismático ajuda tanto na produção texto e no humor da equipe.</div>
+					<br><a href="https://www.instagram.com/_elias.santos_/">Instagram</a>
+				</div>
+				<div class="member">
+					<h4>EVERTON DIAS</h4>
+					<div class="text">Apaixonado por tecnologia e amante de programação, cuida das linhas de código.</div>
+					<br><a href="https://www.instagram.com/everton_days/">Instagram</a>
+				</div>
+				<div class="member">
+					<h4>GABRIEL LOPES</h4>
+					<div class="text">Cheio de ideias tem peça fundamental na area criativa e de marketing do projeto.</div>
+					<br><a href="https://www.instagram.com/gabriel_l.m/">Instagram</a>
+				</div>
+				<div class="member">
+					<h4>IGOR CÂMARA</h4>
+					<div class="text">Foco, determinação e confiança características que faz deste a cabeça pensante do grupo.</div>
+					<br><a href="https://www.instagram.com/i_g_u__/">Instagram</a>
+				</div>
+			</div>
+		</div>
+		<div class="bottom">
+			<a href="https://github.com/Lemon42/snotra-books">feito com ❤️ e PHP</a>
+		</div>
+	</footer>
