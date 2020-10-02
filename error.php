e43a0c8f826b8db345498fc0b938b70466129bc8
Everton Dias <esrdias@gmail.com>
2020-10-02 19:11:27 -0300

    Criando página de erro

---
+++ error.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<!-- Meta Tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Archivo:wght@600;700&family=Work+Sans:wght@600&display=swap" rel="stylesheet">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/global.css">
+	<link rel="stylesheet" href="css/default.css">
+
+	<title>404 - SnotraBooks</title>
+	<link size="25x25px" rel='shortcut icon' href="images/favicon.ico" />
+
+    <style>
+        body{
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-image: url("images/wallpaper-2.jpg");
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 2rem;
+        }
+
+        .wrapper{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            background-color: var(--white);
+            flex-direction: column;
+            padding: 2rem;
+            border-radius: 2rem;
+        }
+
+        h1{
+            font-size: 4.5rem;
+            font-family: Archivo, sans-serif;
+        }
+
+        h2{
+            font-size: 3rem;
+        }
+
+        a{
+            font-size: 2.5rem;
+            color: var(--primary);
+        }
+
+        a:hover{
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h1>Ops!</h1><br><br>
+        <h2>Não tem nada por aqui...</h2><br><br>
+        <a href="index.php">Voltar para a home</a>
+    </div>
+</body>
+</html>
