5c9f2a0d766d21e56f808d1bf987f1a69d65c4b5
Everton Dias <esrdias@gmail.com>
2020-10-02 09:26:16 -0300

    Corrigindo erro ortográfico e melhorando estilo do rodapé

--- models/footer.php
+++ models/footer.php
@@ -10,22 +10,22 @@
 			<hr class="footer-line">
 			<div class="members">
 				<div class="member">
-					<h4>ELIAS SANTOS</h4>
-					<div class="text">Extrovertido e carismático ajuda tanto na produção texto e no humor da equipe.</div>
+					<h4>Elias Santos</h4>
+					<div class="text">Extrovertido e carismático ajuda tanto na produção textual e no humor da equipe.</div>
 					<br><a href="https://www.instagram.com/_elias.santos_/">Instagram</a>
 				</div>
 				<div class="member">
-					<h4>EVERTON DIAS</h4>
-					<div class="text">Apaixonado por tecnologia e amante de programação, cuida das linhas de código.</div>
+					<h4>Everton Dias</h4>
+					<div class="text">Apaixonado por tecnologia e amante de programação, cuida de todas as linhas de código.</div>
 					<br><a href="https://www.instagram.com/everton_days/">Instagram</a>
 				</div>
 				<div class="member">
-					<h4>GABRIEL LOPES</h4>
+					<h4>Gabriel Lopes</h4>
 					<div class="text">Cheio de ideias tem peça fundamental na area criativa e de marketing do projeto.</div>
 					<br><a href="https://www.instagram.com/gabriel_l.m/">Instagram</a>
 				</div>
 				<div class="member">
-					<h4>IGOR CÂMARA</h4>
+					<h4>Igor Câmara</h4>
 					<div class="text">Foco, determinação e confiança características que faz deste a cabeça pensante do grupo.</div>
 					<br><a href="https://www.instagram.com/i_g_u__/">Instagram</a>
 				</div>
